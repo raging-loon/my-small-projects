0953661fabf1b10f74e52eeef21a0cadb21cc945
raging-loon <cxmacolley@gmail.com>
2022-10-14 13:33:16 -0400

    started jws

--- jws/com/jws.java
+++ jws/com/jws.java
@@ -1,14 +1,70 @@
 package com;
+
 import com.jwsConfig.*;
+
+import java.io.*;
+import java.net.*;
 
 public class jws{
 
   private static jwsConfig mainConfiguration;  
+  private static ServerSocket server;
 
+
+  private static void handleRequest(Socket client, String input){
+    
+  }
   public static void main(String[] args) {
     
     mainConfiguration = new jwsConfig();
     mainConfiguration.parseConfigurationFile("jws.conf");
+
     
+    try{
+      server = new ServerSocket(mainConfiguration.getListenPort());
+      while(true){
+        Socket clientSock = server.accept();
+        String ipAddr = clientSock.getInetAddress().toString();
+        ipAddr = ipAddr.substring(ipAddr.indexOf("/") + 1);
+        System.out.println("New connection " + ipAddr + ":" + clientSock.getPort());
+        if(mainConfiguration.isBlockedIPAddr(ipAddr)){
+          System.out.println("Blocking " + ipAddr + " based on configuration. TODO Implement logging");
+          clientSock.close();
+          continue;
+        }
+
+        InputStream input = clientSock.getInputStream();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader((input)));
+        
+        OutputStream output = clientSock.getOutputStream();
+
+        PrintWriter pw = new PrintWriter(output, true);
+        
+        String recv;
+
+        StringBuilder clientInput = new StringBuilder();
+
+        do{
+          recv = reader.readLine();
+          
+          clientInput.append(recv + "\n");
+        
+        } while(!recv.equals(""));      
+
+        handleRequest(clientSock, clientInput.toString());        
+
+        clientSock.close();        
+      }
+
+    } catch (IOException e){
+      System.out.println("Failed to start server");
+      e.printStackTrace();
+      System.exit(1);
+    }
+  
   }
+
+  
+
 }
--- jws/com/jwsConfig.java
+++ jws/com/jwsConfig.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class jwsConfig {
   private String listenIpAddr;
@@ -60,22 +62,66 @@ public class jwsConfig {
 
     if(assignBuffer[0].equals("listen_port"))
       setListenPort(assignBuffer[1]);
-
+    else if(assignBuffer[0].equals("listen_addr"))
+      setListenIpAddr(assignBuffer[1]);
+    else if(assignBuffer[0].equals("root_dir"))
+      setRootDirectory(assignBuffer[0]); 
+    else if(assignBuffer[0].equals("blocked_ips"))
+      setBlockedIPList(assignBuffer[1]); 
   }
 
   private void setListenIpAddr(String ipAddr){
+    Pattern p = Pattern.compile("^(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)\\." +
+    "(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)$");
+    Matcher m = p.matcher(ipAddr);
+    if(!m.matches()){
+      System.out.println("Invalid IP Address: " + ipAddr);
+      System.exit(1);
+    }
     this.listenIpAddr = ipAddr;
   }
 
   private void setListenPort(String listenPort){
     try{
       this.listenPort = Integer.parseInt(listenPort);
-      System.out.println(listenPort);
     } catch(NumberFormatException e){
       System.out.println("Invalid port found at line " + parsedLines);
       System.exit(1);
     }
   }
 
+  private void setRootDirectory(String path){
+    this.rootDirectory = path;
+  }
 
+  private void setBlockedIPList(String ipList){
+    String list = ipList.substring(ipList.indexOf("[") + 1, ipList.indexOf("]"));
+    for(String addr : list.split(",")){
+      if(isValidIPv4Addr(addr))
+        this.blockedIpList.add(addr);
+      else
+        System.out.println("Invalid IPv4 addr "+ addr + ". Skipping");
+    }
+  }
+
+  public static boolean isValidIPv4Addr(String possibleAddress){
+    Pattern p = Pattern.compile("^(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)\\." +
+    "(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)$");
+    Matcher m = p.matcher(possibleAddress);
+
+    return m.matches();
+  }
+
+
+  public boolean isBlockedIPAddr(String ipAddr){
+    return blockedIpList.contains(ipAddr);
+  }
+
+  public String getListenAddr(){ return listenIpAddr; }
+
+  public Integer getListenPort(){ return listenPort; }
+
+
+  
 }
+
